0cb0d9f33b0fd2cfbe2ef2a1d989f6271c60ae19
Lachlan Charlick <lachlan.charlick@gmail.com>
2018-05-11 21:12:35 +0930

    Update setup config

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='metallum',
+    name='python-metallum',
     version='1.0',
     author='Lachlan Charlick',
     author_email='lachlan.charlick@gmail.com',
     url='https://github.com/lcharlick/python-metallum',
     license='LICENSE.md',
     description='Python API for www.metal-archives.com',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     py_modules=['metallum'],
     python_requires='>=3',
     install_requires=['requests', 'requests-cache', 'pyquery', 'python-dateutil']
