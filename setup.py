7a8dfc881312817f191074471def908633b2a79c
Lachlan Charlick <lachlan.charlick@gmail.com>
2018-05-12 11:25:53 +0930

    Fix build issues

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='python-metallum',
-    version='1.0',
+    version='1.0.1',
     author='Lachlan Charlick',
     author_email='lachlan.charlick@gmail.com',
     url='https://github.com/lcharlick/python-metallum',
-    license='LICENSE.md',
+    license='MIT',
     description='Python API for www.metal-archives.com',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     py_modules=['metallum'],
     python_requires='>=3',
     install_requires=['requests', 'requests-cache', 'pyquery', 'python-dateutil']
