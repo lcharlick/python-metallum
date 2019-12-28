cdf9d07fbd7b93a9ed1051f7860e1ff5555704c9
Lachlan Charlick <lachlan.charlick@gmail.com>
2019-12-28 14:36:49 +1030

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-metallum',
-    version='1.0.5',
+    version='1.0.6',
     author='Lachlan Charlick',
     author_email='lachlan.charlick@gmail.com',
     url='https://github.com/lcharlick/python-metallum',
